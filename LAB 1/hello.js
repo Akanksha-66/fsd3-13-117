983d35db12307c3f04fe2b08882097aff423cac9
Akanksha-66 <akankshadixit2024@gmail.com>
2026-07-22 11:49:10 +0530

    commit

---
+++ LAB 1/hello.js
@@ -0,0 +1,19 @@
+function sayHello(name) {
+    console.log(`Welcome to ${name}`);
+}
+const sayHi = (name) => {
+    console.log(`Welcome to ${name}`);
+};
+
+function sum(a,b) {
+    return a+b;
+}
+//const findsum = (a,b) => {
+  //  return a+b;
+//}
+const findsum = (a,b) => a+b;
+
+sayHello('Akanksha Dixit'); 
+sayHi("Akanksha");
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
+console.log(`sum of 2 and 5 is ${findsum(2,5)}`);
