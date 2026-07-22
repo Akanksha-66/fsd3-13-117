92963e17692aceada660eac0e02526170abc9d6d
Akanksha-66 <akankshadixit2024@gmail.com>
2026-07-22 11:53:38 +0530

    commit

--- LAB 1/new.js
+++ LAB 1/new.js
@@ -11,37 +11,37 @@ const NumberToWords = (num) => {
 
 }
 console.log(NumberToWords(5));
-// const towords = (digit) => {
-//     switch(digit) {
-//         case 1 :
-//             return "one";
-//             case 2 :
-//                 return "two";
-//                 case 3:
-//                     return "three";
-//                     case 4 :
-//                         return "four";
-//                         case 5 :
-//                             return "five";
-//                             case 6:
-//                                 return "six";
-//                                 case 7:
-//                                     return "seven";
-//                                     case 8:
-//                                         return "eight";
-//                                         case 9:
-//                                             return "nine";
-//                                             case 0:
-//                                                 return "zero";
-//                                                 default :
-//                                                 return "Number out of range";
-//     }
+const towords = (digit) => {
+    switch(digit) {
+        case 1 :
+            return "one";
+            case 2 :
+                return "two";
+                case 3:
+                    return "three";
+                    case 4 :
+                        return "four";
+                        case 5 :
+                            return "five";
+                            case 6:
+                                return "six";
+                                case 7:
+                                    return "seven";
+                                    case 8:
+                                        return "eight";
+                                        case 9:
+                                            return "nine";
+                                            case 0:
+                                                return "zero";
+                                                default :
+                                                return "Number out of range";
+    }
 
                     
-//     };
-// console.log(towords(5));
-// console.log(towords(3));
-// console.log(towords(7));
+    };
+console.log(towords(5));
+console.log(towords(3));
+console.log(towords(7));
 
 // const rollNum = "250320100117";
 // const digits = String(rollNum).split("");
