6c1d5ab9970e09eaf6f02a3c24cf595336beddc9
Akanksha-66 <akankshadixit2024@gmail.com>
2026-09-09 11:31:10 +0530

    server

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -1,14 +1,15 @@
 import http from 'http'
 
 const server = http.createServer((req,res)=>{
-    if(req.url === '/api/products') {
-        res.end(JSON.stringify({
-            id:1,
+    const products = {
+                       id:1,
             name:"Mobile",
             price: 25000,
              rating:4.5,
              review:200
-        }))
+    };
+    if(req.url === "/api/products") {
+        res.end(JSON.stringify(products));
     }
     else {
         res.statusCode=404;
