70d78ef24a82e86029853e9602edf01fa33b354b
Akanksha-66 <akankshadixit2024@gmail.com>
2026-09-16 23:36:34 +0530

    Add update product

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,28 +1,51 @@
 import http from "http";
-
 const server = http.createServer((req, res) => {
-  if(req.url === '/' && req.method === 'GET') {
+  if (req.url === "/" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("Get Request"); 
-  }
-   else if(req.url === '/' && req.method === 'POST') {
+    res.end("Get Request");
+  } else if (req.url === "/" && req.method === "POST") {
+    // console.log("Request:", req);
+    let body = ''
+    req.on("data", (chunk) => {
+      body += chunk;
+    })
+    req.on("end", () => {
+      const product=JSON.parse(body);
+      console.log("received product:", product);
+      res.statusCode = 201;
+      res.end(
+        JSON.stringify({
+          message: "Product received",
+          receivedProduct: product,
+        }),
+      );
+      
+    });
+    
+  } else if (req.url.startsWith("/products/") && req.method === "PUT") {
+    const productId = req.url.split("/").pop();
+    console.log("Product ID to update:", productId);
+    let body = ''
+    req.on("data", (chunk) => {
+      body += chunk;
+    })
+    req.on("end", () => {
+      const product=JSON.parse(body);
+      console.log("Product updated:", product);
+      res.statusCode = 201;
+      res.end(
+        JSON.stringify({
+          message: "Product updated",
+          updatedProduct: product,
+        }),
+      );
+    });
+  } else if (req.url === "/" && req.method === "DELETE") {
     res.statusCode = 200;
-    res.end("Post Request"); 
-  }
-  else if(req.url === '/' && req.method === 'PUT') {
-    res.statusCode = 200;
-    res.end("Put Request"); 
-  }
-  
-  else if(req.url === '/' && req.method === 'Delete') {
-    res.statusCode = 200;
-    res.end("Delete Request"); 
-  }
-  else {
+    res.end("Delete Request");
+  } else {
     res.statusCode = 404;
-    res.end('request not found')
+    res.end("sent with 404 status code");
   }
-
 });
-
 server.listen(5000, () => console.log("prg6 is running"));
