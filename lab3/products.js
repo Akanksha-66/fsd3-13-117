c3c17ca49b716363eb62f9201d39740493d42ed1
Akanksha-66 <akankshadixit2024@gmail.com>
2026-09-23 11:53:33 +0530

    get all

--- lab3/products.js
+++ lab3/products.js
@@ -1,46 +1,44 @@
 const products = [
-    {"id":1,"name":'marker', "qty":100,"price":15},
-    {"id":2,"name":'pen', "qty":50,"price":20},
-]
+  { id: 1, name: "marker", qty: 100, price: 15 },
+  { id: 2, name: "duster", qty: 50, price: 10 },
+];
+
 let nextId = 3;
 
 export const getAllProducts = () => {
-    return products;
-}
+  return products;
+};
 
 export const addProduct = (item) => {
-      item.id = nextId;
-      nextId++;
-      products.push(item);
-      return item;
+  item.id = nextId;
+  nextId++;
+  products.push(item);
+  return item;
 };
 
 export const deleteProduct = (pid) => {
-      const item = products.findIndex((prd)=>prd.id === pid);
-      if(item == -1)
-        return false;
-    products.splice(item,1)
-    console.log("products remaining:", products);
-    return true;    
+  const item = products.findIndex((prd) => prd.id === pid);
+  if (item == -1) return false;
+  products.splice(item, 1);
+  console.log("products remaining:", products);
+  return true;
 };
 
-// create a function update any product given pid, call this function into prg6.js and verify its working by echo api
-
 export const updateProduct = (pid, updateItem) => {
-  const index = prodcuts.findIndex((prd) => prd.id === pid);
+  const index = products.findIndex((prd) => prd.id === pid);
 
-  if(index == -1){
+  if (index == -1) {
     return false;
   }
   updateItem.id = pid;
-  products[item] = updateItem;
-  return updateItems;
+  products[index] = updateItem;
+  return updateItem;
 };
 
 export const getProductById = (pid) => {
-   const index = prodcuts.findIndex((prd) => prd.id === pid);
+  const index = products.findIndex((prd) => prd.id === pid);
 
-  if(index == -1){
+  if (index == -1) {
     return false;
   }
   return products[index];
